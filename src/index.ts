1447c7ba3b47d08f0878975c028481503c583ad3
Artur Finger <fingerartur@gmail.com>
2023-05-26 13:24:59 +0300

    Get full duration of playlist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 #!/usr/bin/env node
 
-async function main() {
-  console.info('Hello, world!')
-}
+const fs = require('fs');
+
+const secondsToHms = (d: number) => {
+  d = Number(d);
+  const h = Math.floor(d / 3600);
+  const m = Math.floor(d % 3600 / 60);
+  const s = Math.floor(d % 3600 % 60);
+
+  const hDisplay = h > 0 ? h + ':' : '';
+  const mDisplay = m > 0 ? m + ':' : '0:';
+  const sDisplay = s > 0 ? s : '0';
+  return hDisplay + mDisplay + sDisplay;
+};
+
+const main = async () => {
+  const inputFile = process.argv[2];
+  const data = fs.readFileSync(inputFile, { encoding: 'utf8', flag: 'r' });
+
+  const durationTexts = data.split('\n')
+    .filter((line: string) => line.startsWith('#EXTINF'))
+
+  const duration = durationTexts.map((text: string) => {
+    // e.g. #EXTINF:5.500,
+    return Number(text.replace('#EXTINF:', '').replace(',', ''));
+  }).reduce((acc: number, curr: number) => acc + curr, 0);
+
+  console.log('duration sec', secondsToHms(duration));
+};
 
 main()
